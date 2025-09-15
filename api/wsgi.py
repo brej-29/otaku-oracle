4b9584aea7f67969b99080cfd442cdfc79b9849c
brej-29 <brejesh.bala@gmail.com>
2025-09-15 21:47:40 +0530

    api folder deleted

--- api/wsgi.py
+++
@@ -1,7 +0,0 @@
-# api/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "otaku_oracle.settings")
-application = get_wsgi_application()
-app = application  # <-- Vercel needs a top-level 'app'
